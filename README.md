92ca1501b8d314d7aa439f267e7dc7faa9551946
Shun <shun12baseball@gmail.com>
2020-08-06 13:03:11 +0900

    new: goチュートリアル basic4 functionsまで完了

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # go-tutorial
 Go言語 勉強用ディレクトリ
+
+## コマンド
+```
+実行コマンド
+$ go run [file]
+
+ビルド
+$ go build [file]
+```
--- main.go
+++ main.go
@@ -1,11 +1,39 @@
 package main
-
 import (
 	"fmt"
 	"time"
+	"math/rand"
+	"math"
 )
 
+/*
+	functions
+	@see https://tour.golang.org/basics/4
+*/
+func add(x int, y int) int {
+	return x + y
+}
+
 func main() {
+	// print
 	fmt.Println("Hello, World!")
+	// time
 	fmt.Println("The time is", time.Now())
+	/*
+		packages
+		@see https://tour.golang.org/basics/1
+	*/
+	fmt.Println("My favorite number is", rand.Intn(20))
+	/*
+		imports
+		@see https://tour.golang.org/basics/2
+	*/
+	fmt.Println("Now you have problems.\n", math.Sqrt(7))
+	/*
+		exported names
+		@see https://tour.golang.org/basics/3
+	*/
+	fmt.Println(math.Pi)
+	// functions
+	fmt.Println(add(42, 13))
 }
